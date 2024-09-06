9c9e8f070debc9ba5a3f2f0aa3375f0bb7b5c4e3
TSO <tim.sommer@bieber-marburg.de>
2024-09-06 19:17:43 +0200

    Added various moddels / Added Mapping for User

--- mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/smarthome/controller/MappingController.java
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/smarthome/controller/MappingController.java
@@ -1,15 +1,25 @@
 package mosbach.dhbw.de.smarthome.controller;
 
-import org.aspectj.bridge.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import mosbach.dhbw.de.smarthome.model.AuthMessage;
+import mosbach.dhbw.de.smarthome.model.MailTokenRequest;
 import mosbach.dhbw.de.smarthome.model.MessageAnswer;
 import mosbach.dhbw.de.smarthome.model.MessageReason;
 import mosbach.dhbw.de.smarthome.model.MessageToken;
+import mosbach.dhbw.de.smarthome.model.User;
+import mosbach.dhbw.de.smarthome.model.UserChangeRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -21,6 +31,12 @@ public class MappingController {
 
     }
 
+
+    
+    //######################################################
+    //AUTH
+    //######################################################
+
     @GetMapping("/auth")
     public String getAuth() {
         return "I am alive.";
@@ -30,11 +46,11 @@ public class MappingController {
         path = "/auth",
         consumes = {MediaType.APPLICATION_JSON_VALUE}
     )
-    public ResponseEntity<?> signIn(@RequestBody AuthMessage authMessage) {
+    public ResponseEntity<?> signIn(@RequestBody AuthMessage authMessage) { //Sign in implementation
         if(authMessage.getEmail().equals("test") && authMessage.getPasswort().equals("test")) {
             return new ResponseEntity<MessageToken>(new MessageToken("1234567890"), HttpStatus.OK);
         } else {
-            return new ResponseEntity<MessageAnswer>(new MessageAnswer("Wrong credentials"), HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<MessageAnswer>(new MessageAnswer("Wrong credentials"), HttpStatus.BAD_REQUEST);
         }
     }
     
@@ -42,11 +58,82 @@ public class MappingController {
         path = "/auth",
         consumes = {MediaType.APPLICATION_JSON_VALUE}
     )
-    public ResponseEntity<?> signOut(@RequestBody MessageToken messageToken) {
+    public ResponseEntity<?> signOut(@RequestBody MessageToken messageToken) { //Sign out implementation
         if(messageToken.getToken() == "1234567890") {
             return new ResponseEntity<MessageAnswer>(new MessageAnswer("Logout successful"), HttpStatus.OK);
         } else {
-            return new ResponseEntity<MessageReason>(new MessageReason("Logout failed"), HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<MessageReason>(new MessageReason("Logout failed"), HttpStatus.BAD_REQUEST);
         }
      }
+
+
+     //######################################################
+     //user
+     //######################################################
+
+    @GetMapping(
+        path = "/user",
+        consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<?> getUser(@RequestBody MailTokenRequest mailTokenRequest) { //TODO: Get User Implementation
+        if(mailTokenRequest.getToken() == "123456" && mailTokenRequest.getEmail() == "test@test.com"){
+            return new ResponseEntity<User>(new User("Tim", "Sommer", "test@test.com", "pw"), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<MessageReason>(new MessageReason("Wrong Credetials"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(
+        path = "/user",
+        consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<?> createUser(@RequestBody User user) { //TODO: Create User Implementation
+        if(user.getEmail() != "test@test.com"){
+            return new ResponseEntity<MessageAnswer>(new MessageAnswer("Account created"), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<MessageReason>(new MessageReason("Mail already exists"), HttpStatus.BAD_REQUEST);
+        }
+    }
+    
+    @DeleteMapping(
+        path = "/user",
+        consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<?> deleteUser(@RequestBody MailTokenRequest mailTokenRequest) { //TODO: Delete User Implementation
+        if(mailTokenRequest.getToken() == "123456" && mailTokenRequest.getEmail() == "test@test.com"){
+            return new ResponseEntity<MessageAnswer>(new MessageAnswer("Account deleted"), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<MessageReason>(new MessageReason("Wrong Credetials"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<?> changeUser(@RequestBody UserChangeRequest changeRequest) {
+        //TODO: process PUT request
+        if (changeRequest.getToken() == "123456") {
+            return new ResponseEntity<MessageAnswer>(new MessageAnswer("Account updated"), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<MessageReason>(new MessageReason("Wrong Token"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    
+    //######################################################
+     //device
+     //######################################################
+     
+
+     
+
+
+
+     //######################################################
+     //routine
+     //######################################################
+
+     
 }
---
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/smarthome/model/Device.java
@@ -0,0 +1,84 @@
+
+package mosbach.dhbw.de.smarthome.model;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Device {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("location")
+    private String location;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Device() {
+    }
+
+    /**
+     * 
+     * @param name
+     * @param location
+     * @param type
+     */
+    public Device(String name, String type, String location) {
+        super();
+        this.name = name;
+        this.type = type;
+        this.location = location;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("location")
+    public String getLocation() {
+        return location;
+    }
+
+    @JsonProperty("location")
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
