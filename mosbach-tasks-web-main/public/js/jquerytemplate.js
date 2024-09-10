bc972b20ade82e5fcffa357f549ac8107b2b52fd
Chris-Laws <Chriskaufmann11@gmail.com>
2024-09-10 10:19:32 +0200

    AddNewDevice Maske & JSON

---
+++ mosbach-tasks-web-main/public/js/jquerytemplate.js
@@ -0,0 +1,50 @@
+	
+<script>
+
+$("#tasksubmit").click(function() {
+
+    var taks = {
+
+            name: $("#taskname").val(),
+
+            description:$("#taskdescription").val(),
+
+            priority:parseInt($("#taskpriority").val())
+
+        };
+
+    console.log(task);
+
+    $.ajax({
+
+        url: 'https://agile-wave-43715.herokuapp.com/api/v1.0/task',
+
+        type: 'post',
+
+        dataType: 'json',
+
+        contentType: 'application/json',
+
+        success: function (data) {
+
+            alert(data);
+
+        },
+
+        data: JSON.stringify(task),
+
+        processData: false,
+
+        contentType: "application/json; charset=UTF-8",
+
+        error: function (xhr, ajaxOptions, thrownError) {
+
+            alert('Error: ' + xhr.status + '   ' + thrownError);
+
+          }
+
+    });
+
+});
+
+</script>
