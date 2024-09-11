e18e9feadc7e4333c77a59fef99a62a3de70d514
TSO <tim.sommer@bieber-marburg.de>
2024-09-11 08:58:01 +0200

    Passwort Encoding added

--- mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/smarthome/model/User.java
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/smarthome/model/User.java
@@ -1,17 +1,27 @@
 package mosbach.dhbw.de.smarthome.model;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class User {
     private String firstName;
     private String lastName;
     private String email;
     private String passwort;
 
+    
+    private static PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
     public User(String firstName, String lastName, String email, String passwort) {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.passwort = passwort;
+        this.passwort = passwordEncoder.encode(passwort);
+    }
+
+    public boolean checkPasswort(String pw){
+        return passwordEncoder.matches(pw, this.passwort);
     }
 
     // Getter and Setter
