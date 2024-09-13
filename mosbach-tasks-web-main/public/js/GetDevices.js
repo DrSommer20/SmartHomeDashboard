607e63dadc2e66f0caf97e95d442a0efbe8ad65c
paulib24 <bauerpaula03@gmail.com>
2024-09-13 11:21:52 +0200

    device list display

--- mosbach-tasks-web-main/public/js/GetDevices.js
+++ mosbach-tasks-web-main/public/js/GetDevices.js
@@ -13,3 +13,36 @@ $(document).ready(function() {
         }
     });
 });
+
+function displayDevices(devices) {
+    const contentDiv = document.getElementById('content'); 
+    devices.forEach(device => {
+        const deviceDiv = document.createElement('div');
+        deviceDiv.classList.add('device');
+        deviceDiv.innerHTML = `
+            <div class="device-header">
+                <h3>
+                    <span class="material-symbols-outlined">${getIcon(device.type)}</span> ${device.name}
+                </h3>
+            </div>
+            <div class="device-separator"></div>
+            <div class="device-info">
+                <p>Type: ${device.type}</p>
+                <p>Location: ${device.location}</p>
+                <p>State: ${device.state}</p>
+                <br />
+                <button class="device-button">${device.state === 'On' ? 'Turn Off' : 'Turn On'}</button>
+            </div>
+        `;
+        contentDiv.appendChild(deviceDiv);
+    });
+}  
+    //Symbole
+    function getDeviceIcon(type) {
+        switch(type.toLowerCase()) {
+            case 'light': return 'light';
+            case 'outlet': return 'outlet';
+            case 'lamp': return 'table_lamp';
+            default: return 'device_unknown';
+        }
+    }
