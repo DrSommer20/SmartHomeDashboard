47298fd843765a79a52bf880925661d4c678326e
Tim <151786923+DrSommer20@users.noreply.github.com>
2024-10-04 20:14:14 +0200

    Added SPA Template

---
+++ smarthome-dashboard-spa/src/app/content-header/content-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'an-content-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-header.component.html',
+  styleUrl: './content-header.component.css'
+})
+export class ContentHeaderComponent {
+
+}
