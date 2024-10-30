68d27c6944380d786a690ffada706a9c97ea70ad
paulib24 <bauerpaula03@gmail.com>
2024-10-30 09:49:51 +0100

    on off routine function

--- smarthome-dashboard-web/public/js/GetRoutines.js
+++ smarthome-dashboard-web/public/js/GetRoutines.js
@@ -87,9 +87,44 @@ function displayRoutine(routines) {
            }
        );
    }
-   function handleChange(isChecked, device_id, uniqueId, checkboxElement){
 }
+   function handleChange(isChecked, device_id, uniqueId, checkboxElement){
+         $.ajax({
+              url: 'https://smarthomebackend-spontaneous-bilby-ni.apps.01.cf.eu01.stackit.cloud/api/routine/' + device_id + '/'+ (isChecked ? 'on' : 'off'),
+              type: 'POST',
+              headers: {
+                'Authorization': localStorage.getItem('authToken')
+            },
+              success: function(response) {
+                onSuccess(device_id, uniqueId);
+              },
+              error: function(error) {
+                console.error('Error changing routine state:', error);
+              }
+         });
+   }
 
+function onSuccess(device_id, uniqueId){
+$.ajax({
+        url: 'https://smarthomebackend-spontaneous-bilby-ni.apps.01.cf.eu01.stackit.cloud/api/routine/' + device_id,
+        type: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('authToken'),
+            'Content-Type': 'application/json'
+        },
+        success: function(response) {
+            const checkbox = document.getElementById(uniqueId);
+            if (response.state === true) {
+                checkbox.checked = true;
+            } else {
+                checkbox.checked = false;
+            }
+        },
+        error: function(error) {
+            console.error('Fehler bei der Anfrage:', error);
+        }
+    });
+}
 //Edit Routine
 const devices = [];
 
@@ -101,7 +136,6 @@ $(document).ready(function() {
             'Authorization': localStorage.getItem('authToken')
         },
         success: function (response) {
-            console.log('Geräte erfolgreich abgerufen:', response);
             response.devices.forEach(device => {
                 devices.push(device); 
             });
@@ -233,10 +267,6 @@ $('#RoutinesaveBtn').click(function (id) {
 });
 
 
-//TODO: Device schaltfunktion einfügen
-// ist im Backend was, dass zu der Zeit was geschaltet wird?
-//wann und für was genau gibt man dann die Toggle function durch?
-
 //Delete Routine
 function deletebuttonclick(id){
     $('.deletepopup').css('display', 'flex');
@@ -262,4 +292,4 @@ function deletebuttonclick(id){
         $('.deletepopup').css('display', 'none');
     });
 }
-};
+
